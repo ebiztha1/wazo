70ae5a3e7b2b2441f66f81983aea49f7251f0a35
ebony walker <eb.walk@gmail.com>
2014-02-07 12:34:28 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WazoApp::Application.config.secret_key_base = '3633a0cfe41d33d933bc00549664d0ab6b0af35e50131ac3427317568434a915ffb376aab5a234331b3c91374f2441ecd4fec167df02cd1673c13b11eb821bd9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WazoApp::Application.config.session_store :cookie_store, key: '_wazo_app_session'
